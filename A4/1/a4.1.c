436ce1645e87d3fe168ebe2bbdf151070eb135a7
sanyuchilas <sanyuchilas@gmail.com>
2024-05-20 07:45:05 +0300

    fixed

--- A4/1/a4.1.c
+++ A4/1/a4.1.c
@@ -50,29 +50,29 @@ void test(unsigned char A[16]) {
 
     "loop_start:           \n"
         "mov al, [esi];    \n"
-        "inc esi;           \n"
+        "inc esi;          \n"
         "mov dl, [esi];    \n"
 
-        "sub al, dl;      \n"
+        "sub al, dl;       \n"
         "js negative;      \n"
 
-        "mov [edi], al;     \n"
+        "mov [edi], al;    \n"
         "jmp loop_continue \n"
     
-    "negative:    \n"
-        "mov [ebx], edi; \n"
+    "negative:             \n"
+        "mov [ebx], edi;   \n"
 
-        "inc ebx; \n"
-        "inc %0; \n"
+        "inc ebx;          \n"
+        "inc %0;           \n"
 
         "jmp loop_continue \n"
     
-    "loop_continue: \n"
-        "inc edi;           \n"
+    "loop_continue:        \n"
+        "inc edi;          \n"
         "dec ecx;          \n"
 
-        "cmp ecx, 0;         \n"
-        "jne loop_start     \n"
+        "cmp ecx, 0;       \n"
+        "jne loop_start    \n"
 
         : "=m" (CLength)
         : "m" (A), "m" (B), "m" (C)
@@ -88,9 +88,16 @@ void test(unsigned char A[16]) {
 
 int main() {
     // Test 1.
-    unsigned char A[16] = {13, 12, 13, 15, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    unsigned char A1[16] = {13, 12, 13, 15, 1, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    test(A1);
 
-    test(A);
+    // Test 2.
+    unsigned char A2[16] = {5, 8, 0, 3, 1, 1, 2, 6, 4, 18, 11, 7, 9, 6, 3, 1};
+    test(A2);
+
+    // Test 3.
+    unsigned char A3[16] = {0, 1, 2, 7, 3, 6, 4, 5, 5, 0, 12, 13, 5, 1, 3, 1};
+    test(A3);
 
     return 0;
 }
